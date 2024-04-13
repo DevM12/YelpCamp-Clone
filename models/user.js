31227ff3697c492559053f7335e1d4ae6502d67e
Dev Malhotra <153187417+Daymmnn@users.noreply.github.com>
2024-04-13 18:14:17 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema= mongoose.Schema;
-const passportLocalMongoose = require('passport-Local-Mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     email:{
