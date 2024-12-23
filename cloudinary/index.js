e176ae3f69a3ca53c9fb65e8a69c92d883a27b10
Dev Malhotra <153187417+DevM12@users.noreply.github.com>
2024-12-23 23:54:56 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -7,7 +7,6 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
-
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
