fec8ae7b04a88d62ef5e7ff651035583ff960345
Dev Malhotra <153187417+DevM12@users.noreply.github.com>
2024-12-23 23:58:02 +0530

    Update validateForm.js

--- public/javascript/validateForm.js
+++ public/javascript/validateForm.js
@@ -5,7 +5,6 @@
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.validated-form')
-
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
