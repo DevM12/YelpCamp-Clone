1fb33ce308305a9121f55c99ac50d315f462357f
Dev Malhotra <153187417+DevM12@users.noreply.github.com>
2024-12-23 23:55:52 +0530

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -11,7 +11,6 @@ module.exports.createReview= async (req, res) => {
     req.flash('success', 'Successfully created a new Review');
     res.redirect(`/campgrounds/${campground._id}`);
 }
-
 module.exports.deleteReview = async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
