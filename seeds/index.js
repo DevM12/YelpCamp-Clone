ae34bbb51ba27a7f4054fc5c3f62ea8511d6568a
Dev Malhotra <153187417+DevM12@users.noreply.github.com>
2024-12-24 00:00:33 +0530

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -7,7 +7,6 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
